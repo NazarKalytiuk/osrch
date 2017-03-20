4b2b8d3cf6b4e8dc146cb2b579150ce790184857
SergiyRomanchuk <serg.roman4yk@gmail.com>
2017-03-20 23:37:59 +0300

    Create db and execute queries L2 ended

--- L2/BankDomain/EXECUTEFILE.py
+++ L2/BankDomain/EXECUTEFILE.py
@@ -1,4 +1,9 @@
 from BankDomain.Investor import Investor
+from BankDomain.DBInitializer import DbInitializer
+
+initializer = DbInitializer.inst()
+
+initializer.CreateTables();
 
 inv = Investor()
 inv.InvestorCode = 1
@@ -6,3 +11,4 @@ inv.Pib = '''Sergiy Romanchuk Anat'''
 
 inv.InvestorCode += 3
 print(str(inv.InvestorCode) + '-' + inv.Pib)
+
